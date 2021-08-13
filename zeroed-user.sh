cc5f227ee38557567512ee562d154e031e5b794c
Gustavo Teixeira <iwhrim@gmail.com>
2021-08-13 16:48:51 -0300

    Update zeroed-user.sh

--- zeroed-user.sh
+++ zeroed-user.sh
@@ -1,12 +1,37 @@
-# First
-pacman -S --noconfirm xorg xorg-xinit xorg-xprop xorg-xrandr xorg-server nvidia nvidia-utils nvidia-settings
+/# First
+pacman -S --noconfirm xorg xorg-xinit xorg-xprop xorg-xrandr xorg-server nvidia nvidia-utils nvidia-settings base-devel
 cp /etc/X11/xinit/xinitrc .xinitrc
 
 # Second
-sudo pacman -S i3-gaps dmenu picom firefox terminator
+sudo pacman -S i3-gaps i3status dmenu picom firefox terminator git nitrogen pulseaudio pulseaudio-alsa pavucontrol 
  vim .xinitrc
  delete last five lines
  append "exec i3"
+ 
+ 
+ # Polybar
+ sudo pacman -S xorg-fonts-misc
+ yay -S siji-git ttf-unifont
+ yay polybar
+ mkdir .config/polybar
+ # TODO: Trocar pela minha config
+ sudo cp /usr/share/doc/polybar/config ~/.config/polybar/
+ sudo chown teixeira:users .config/polybar/config
+ 
+ # TODO criar script para https://github.com/polybar/polybar/wiki#launching-the-bar-in-your-wms-bootstrap-routine
+ # https://github.com/polybar/polybar/wiki#launching-the-bar-in-your-wms-bootstrap-routine
+ sudo chmod +x .config/polybar/launch.sh
+
+
+
+ 
+ 
+ 
+ 
+ Verificar...
+ https://github.com/loeken/dotfiles/blob/master/i3/config
+ 
+ 
  
 
 # Fonts

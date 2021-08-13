58576b24e354165f7359a7a1163e6f0bd5e5db55
Gustavo Teixeira <iwhrim@gmail.com>
2021-08-13 15:33:42 -0300

    Update zeroed-user.sh

--- zeroed-user.sh
+++ zeroed-user.sh
@@ -1,6 +1,13 @@
-# Xorg
-sudo pacman -S --noconfirm xorg xorg-xinit xorg-prop xorg-xrandr
+# First
+pacman -S --noconfirm xorg xorg-xinit xorg-xprop xorg-xrandr xorg-server nvidia nvidia-utils nvidia-settings
 cp /etc/X11/xinit/xinitrc .xinitrc
+
+# Second
+sudo pacman -S i3-gaps dmenu picom firefox terminator
+ vim .xinitrc
+ delete last five lines
+ append "exec i3"
+ 
 
 # Fonts
 sudo pacman -S --noconfirm firefox dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji

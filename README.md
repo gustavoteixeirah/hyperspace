eaabba4a6e701bc10745acf384d431e4d98046b6
Gustavo Teixeira <iwhrim@gmail.com>
2021-09-04 21:48:26 -0300

    another commit

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
 # Arch Installation & Scripts
 
 
-./nbminer -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u hyperspace.001 --api 127.0.0.1:22333 -lhr 68%
+Carrega keymaps 
+```
+loadkeys br-abnt2
+```
+Ajusta fuso
+```
+timedatectl set-ntp true
+```
+ Confere se vc tem conexão com a internet:
+```
+ip a
+```
+
+Confere sincronização com repo dos pacotes
+```
+pacman -Sy
+```
+Confere discos
+```
+lsblk
+```
+Particiona conforme preferir. Recomendado usar cfdisk. GPT partition. EXEMPLO:
+```
+sda1 - partição para /mnt/boot, recomendado 512M
+sda2 - partição para /mnt, recomendado 32G
+sda3 - partição para /mnt/home, recomendado o que sobrar
+```
+Formatar partição do boot:
+```
+mkfs.fat -F32 /dev/sda1
+```
+Formatar outra partições:
+```
+mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda3
+```
+Mount...
+```
+mount /dev/sda2 /mnt
+mkdir /mnt/{home,boot}
+mount /dev/sda1 /mnt/boot
+mount /dev/sda3 /mnt/home
+```
+Iniciar instalação
+```
+pacstrap /mnt base linux linux-firmware git vim amd-ucode
+```
+Gera filesystem table
+```
+genfstab -U /mnt >> /mnt/etc/fstab
+```
+Change to root user on the new installed system
+```
+arch-chroot /mnt /bin/bash
+```
+Muda senha do usuário root
+```
+passwd
+```
+Clona repo e executa o base.sh
--- base.sh
+++ base.sh
@@ -2,8 +2,7 @@
 
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
-timedatectl set-ntp true
-sed -i '177s/.//' /etc/locale.gen
+sed -i '393s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
@@ -11,39 +10,19 @@ echo "hyperspace" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 localhost.localdomain hyperspace" >> /etc/hosts
-# echo root:password | chpasswd
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
-
-# pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
-
-pacman -S grub efibootmgr linux-headers networkmanager base-devel sudo git
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-
+pacman -S grub efibootmgr linux-headers networkmanager base-devel sudo nvidia nvidia-utils nvidia-settings
+ 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
-# systemctl enable bluetooth
-# systemctl enable cups.service
-# systemctl enable sshd
-# systemctl enable avahi-daemon
-# systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-# systemctl enable reflector.timer
-# systemctl enable fstrim.timer
-# systemctl enable libvirtd
-# systemctl enable firewalld
-# systemctl enable acpid
 
 useradd -m  -g users -G wheel -s /bin/bash teixeira
-# echo ermanno:password | chpasswd
-# usermod -aG libvirt ermanno
+echo teixeira:teixeira | chpasswd
+echo "teixeira ALL=(ALL) ALL" >> /etc/sudoers.d/teixeira
 
-# echo "ermanno ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
-
-
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+printf "\e[1;32mDone! Type exit, umount -R /mnt and reboot.\e[0m"
 
 
 # Reference:

0e5553bc9563b0142dad929b39d90e274ebb8251
Gustavo Teixeira <contact@gustavoteixeira.dev>
2023-07-26 00:24:16 -0300

    add  poetry to install list

--- 1_dev.sh
+++ 1_dev.sh
@@ -10,7 +10,7 @@ for x in "${aurpackages[@]}"; do
 done
 
 # General tools
-sudo pacman --noconfirm --needed -S curl wget zip unzip neofetch btop python-pip obsidian
+sudo pacman --noconfirm --needed -S curl wget zip unzip neofetch btop  obsidian
 
 # IDEs
 sudo pacman --noconfirm --needed -S intellij-idea-community-edition
@@ -24,9 +24,12 @@ sudo pacman --noconfirm --needed -S ttf-fira-code noto-fonts noto-fonts-emoji tt
 # Frontend related
 sudo pacman --noconfirm --needed -S nodejs npm pnpm
 
+# Python related
+sudo pacman --noconfirm --needed -S python-pip python-poetry
+poetry config virtualenvs.in-project true
+
 # Infra tools
 sudo pacman --noconfirm --needed -S docker docker-compose 
-
 systemctl start docker.service
 systemctl enable docker.service
 sudo usermod -aG docker "$USER"

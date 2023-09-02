e6412e357ce2eee6349278ae981da93bb189e0ad
Gustavo Teixeira <contact@gustavoteixeira.dev>
2023-09-02 14:56:17 -0300

    Update 2_code.sh

--- 2_code.sh
+++ 2_code.sh
@@ -4,7 +4,9 @@ git config --global core.editor "vim"
 git config --global user.email "contact@gustavoteixeira.dev"
 git config --global user.name "Gustavo Teixeira"
 git config --global url.git@github.com:.insteadOf https://github.com/
-git config --global --add --bool push.autoSetupRemote true
+git config --global --add --bool push.autoSetupRemote true 
+git config --global pull.rebase false
+
 
 ssh-keygen -t ed25519 -C "contact@gustavoteixeira.dev"
 eval "$(ssh-agent -s)"

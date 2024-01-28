b2c5c992d90e82be32c4b756b5f4c45bfd523c56
Gustavo Teixeira <contact@gustavoteixeira.dev>
2024-01-27 23:50:16 -0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+!/usr/bin/bash
+
+sudo pacman --noconfirm --needed -S  noto-fonts noto-fonts-emoji ttf-dejavu ttf-fira-code
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+sudo cp -v ~/code/hyperspace/arch/dotfiles/.zshrc ~/.zshrc
+sudo cp -v ~/code/hyperspace/arch/oh-my-zsh-themes/* ~/.oh-my-zsh/custom/themes/

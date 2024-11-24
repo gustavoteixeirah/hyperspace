1e763090281a78cd3d0484207c592da188607da9
Gustavo Teixeira <contact@gustavoteixeira.dev>
2024-11-23 22:32:39 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 !/usr/bin/bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 sudo pacman --noconfirm --needed -S  noto-fonts noto-fonts-emoji ttf-dejavu ttf-fira-code
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting

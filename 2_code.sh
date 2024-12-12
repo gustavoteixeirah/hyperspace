6147df0c0602052df65dd98681fd6f058b05d529
Gustavo Teixeira <10344032+gustavoteixeirah@users.noreply.github.com>
2024-12-12 01:25:41 -0300

    asdf

--- 2_code.sh
+++ 2_code.sh
@@ -1,14 +1,14 @@
 !/usr/bin/bash
 
 git config --global core.editor "vim"
-git config --global user.email "contact@gustavoteixeira.dev"
+git config --global user.email "10344032+gustavoteixeirah@users.noreply.github.com"
 git config --global user.name "Gustavo Teixeira"
 git config --global url.git@github.com:.insteadOf https://github.com/
 git config --global --add --bool push.autoSetupRemote true 
 git config --global pull.rebase false
 
 
-ssh-keygen -t ed25519 -C "contact@gustavoteixeira.dev"
+ssh-keygen -t ed25519 -C "10344032+gustavoteixeirah@users.noreply.github.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 

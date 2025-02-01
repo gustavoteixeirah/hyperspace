e1527950b6c9b035263bf60e3c9b54b4f37052b5
Gustavo Teixeira <10344032+gustavoteixeirah@users.noreply.github.com>
2025-01-31 22:46:18 -0300

    mais ajustes

--- 1_dev.sh
+++ 1_dev.sh
@@ -25,15 +25,12 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 #sudo pacman --noconfirm --needed -S java-runtime-common jdk17-openjdk maven gradle
 
-# Fonts
-sudo pacman --noconfirm --needed -S ttf-fira-code noto-fonts noto-fonts-emoji ttf-dejavu
-
 # Frontend related
-sudo pacman --noconfirm --needed -S nvm nodejs npm pnpm 
+# sudo pacman --noconfirm --needed -S nvm nodejs npm pnpm 
 
 # Python related
 # sudo pacman --noconfirm --needed -S tk python-pip python-ipykernel python-minio jupyterlab jupyter-notebook cuda tensorflow-cuda python-tensorflow-cuda pyenv ruff
-curl -LsSf https://astral.sh/uv/install.sh | sh
+# curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Rust related
 # jsudo pacman --noconfirm --needed -S rustup 
@@ -48,9 +45,6 @@ sudo systemctl start docker.service
 sudo systemctl enable docker.service
 sudo usermod -aG docker "$USER"
 newgrp docker
-
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 sudo pacman -S $(pacman -Sgq nerd-fonts)
 
--- asdf.sh
+++ asdf.sh
@@ -5,11 +5,7 @@
 # git clone https://aur.archlinux.org/asdf-vm.git && cd asdf-vm && makepkg -si
 
 
-asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+#asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
+#asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin-add java https://github.com/halcyon/asdf-java.git
-asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
-asdf plugin-add python
-asdf install python 3.12.0
-asdf plugin add uv https://github.com/b1-luettje/asdf-uv.git
-asdf global python 3.12.0
+asdf plugin-add golang https://github.com/asdf-community/asdf-golang.git

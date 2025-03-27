59c4cd5d7cb047e2db6509640c1d306d0d916ab4
Gustavo Teixeira <10344032+gustavoteixeirah@users.noreply.github.com>
2025-03-26 22:26:19 -0300

    Update 0_ui.sh

--- 0_ui.sh
+++ 0_ui.sh
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-sudo pacman --noconfirm --needed -S xorg xorg-server xorg-xinit xorg-xprop xorg-xrandr arandr xorg-fonts-misc rofi feh pavucontrol 
+sudo pacman --noconfirm --needed -S xorg xorg-server xorg-xinit xorg-xprop xorg-xrandr arandr xorg-fonts-misc rofi feh pavucontrol wezterm i3 base-devel rofi feh curl wget zip unzip zsh zsh-completions neofetch ranger fzf zoxide noto-fonts noto-fonts-emoji ttf-dejavu ttf-fira-code 
 # sudo pacman --noconfirm --needed -S nvidia-lts  
 sudo pacman --noconfirm --needed -S i3 
-sudo cp ~/code/hyperspace/dotfiles/.xinitrc ~/.xinitrc
-sudo mkdir -p ~/.local/share/fonts
-sudo cp ~/code/hyperspace/fonts/* ~/.local/share/fonts
-fc-cache
+#sudo cp ~/code/hyperspace/dotfiles/.xinitrc ~/.xinitrc
 
 #sudo pacman --noconfirm --needed -S xorg xorg-server xorg-xinit xorg-xprop xorg-xrandr arandr xorg-fonts-misc rofi feh pavucontrol 
 # sudo pacman --noconfirm --needed -S nvidia-lts  
@@ -16,15 +12,7 @@ fc-cache
 #fc-cache
 
 #Install st
-cd ~/code/hyperspace/arch/suckless/st || exit
-sudo make install
-cd ~ || exit
-# Tools
-sudo pacman --noconfirm --needed -S base-devel rofi feh curl wget zip unzip zsh zsh-completions neofetch ranger fzf zoxide
-# Fonts
-sudo pacman --noconfirm --needed -S noto-fonts noto-fonts-emoji ttf-dejavu ttf-fira-code 
-# Audio
-sudo pacman --noconfirm --needed -S pulseaudio pavucontrol
+
 chsh -s `which zsh`
 git clone https://aur.archlinux.org/yay.git
 cd yay

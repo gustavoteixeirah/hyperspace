720d60f5cf4ef76ea48b70fbec59480976edc3e2
Gustavo Teixeira <10344032+gustavoteixeirah@users.noreply.github.com>
2025-07-23 02:52:58 -0300

    added more programs
    added more installations programs based on omarchy

--- 1_dev.sh
+++ 1_dev.sh
@@ -17,6 +17,35 @@ done
 sudo pacman --noconfirm --needed -S ufw tmux curl wget zip unzip tree jq btop openssh fd keepass lsd bat flameshot 
 sudo pacman -S openssl readline zlib curl libutil-linux icu
 
+yay -S --noconfirm --needed \
+  brightnessctl playerctl pamixer pavucontrol wireplumber \
+  fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
+  nautilus sushi ffmpegthumbnailer \
+  mpv evince imv
+
+# Install bluetooth controls
+yay -S --noconfirm --needed blueberry
+
+# Turn on bluetooth by default
+sudo systemctl enable --now bluetooth.service
+
+yay -S --noconfirm power-profiles-daemon
+
+yay -S --noconfirm --needed libreoffice obs-studio
+
+yay -S --noconfirm --needed pinta ||
+  echo -e "\e[31mFailed to install Pinta. Continuing without!\e[0m"
+
+yay -S --noconfirm --needed spotify dropbox-cli 1password-beta 1password-cli ||
+  echo -e "\e[31mFailed to install Spotify, Dropbox, and/or 1password because of a keyserver problem. Continuing without!\e[0m"
+
+yay -S --noconfirm --needed \
+  cargo clang llvm mise \
+  imagemagick \
+  mariadb-libs postgresql-libs \
+  github-cli \
+  lazygit lazydocker-bin
+
 
 # Neovim dependencies
 sudo pacman --noconfirm --needed -S ripgrep xclip xsel lua-language-server
